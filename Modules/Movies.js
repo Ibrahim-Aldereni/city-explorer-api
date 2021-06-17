71255b6cb51b4442d803fe5e4bf9893446cdc51e
Ibrahim-Aldereni <ibrahemaldrynee@gmail.com>
2021-06-17 12:36:51 +0300

    add memory varaible to save movies

--- Modules/Movies.js
+++ Modules/Movies.js
@@ -2,23 +2,31 @@
 
 const axios = require("axios");
 
+let memory = {};
+
 function moviesFromApi(req, res) {
   let cityName = req.query.searchQuery;
   let key = process.env.MOVIE_API_KEY;
 
-  let url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${cityName}`;
-
-  axios
-    .get(url)
-    .then((response) => {
-      let result = response.data.results.map((item) => {
-        return new Data(item);
+  if (memory[cityName] !== undefined) {
+    res.status(200).send(memory[cityName]);
+    console.log("from memory");
+  } else {
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${cityName}`;
+    axios
+      .get(url)
+      .then((response) => {
+        let result = response.data.results.map((item) => {
+          return new Data(item);
+        });
+        memory[cityName] = result;
+        result.length == 0 ? res.send("No movies") : res.send(result);
+        console.log("from API");
+      })
+      .catch((err) => {
+        res.status(500).send("movies not found in this city");
       });
-      result.length == 0 ? res.send("No movies") : res.send(result);
-    })
-    .catch((err) => {
-      res.status(500).send("movies not found in this city");
-    });
+  }
 }
 
 class Data {
